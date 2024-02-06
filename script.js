6ce40564dffff38feba8041c067bbad413075745
eduardo <eduardogomesk123@live.com>
2024-02-06 15:19:06 -0300

    jquarry-apostila

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     
     $("#item_caixa_1").show(); 
+    $("#a_ser_mostrado").hide();
 
     $("#botao_icones_1").click(function() {
      
@@ -28,6 +29,44 @@ $(document).ready(function() {
         $("#item_caixa_3").show(); 
   
       });
+
+
+      $("#caixa_alert").click(function() {
+          alert('deu certo');
+
+       
+      });
+
+
+      $("#caixa_alert2").dblclick(function() {
+        alert('deu certo');
+
+     
+    });
+
+
+    $("#botao_que_vai_esconder").click(function() {
+      $("#a_ser_escondido").hide();
+     
+    
+    });
+
+   
+   
+    $("#botao_que_vai_mostrar").click(function() {
+      $("#a_ser_mostrado").slideDown(2000);
+     
+    
+    });
+
+
+    $("#botao_que_vai_mostrar").click(function() {
+      $("#a_ser_mostrado").slideDown(2000);
+     
+    
+    });
+    
+
 
 
   });
